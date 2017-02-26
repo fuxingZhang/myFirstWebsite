d30cb6beb8c526ca0c542b04fc69b55f0536b817
fuxingZhang <502545703@qq.com>
2017-02-26 23:41:32 +0800

    201702262341

--- js/zfx.js
+++ js/zfx.js
@@ -6,4 +6,18 @@ $(function(){
 	},function(){
 		$(this).find("ul").slideUp();
 	});
+
+	// banner  横幅; 标语; 旗，旗帜; 大字标题;
+	$(".banner").hover(function () {
+        $(this).find(".prev,.next").fadeTo("show", 0.1);
+    }, function () {
+        $(this).find(".prev,.next").hide();
+    })
+    $(".prev,.next").hover(function () {
+        $(this).fadeTo("show", 0.7);
+    }, function () {
+        $(this).fadeTo("show", 0.1);
+    })
+    $(".banner").slide({ titCell: ".num ul", mainCell: ".51buypic", effect: "left", autoPlay: true, delayTime: 700, autoPage: "<li><a></a></li>" });
+
 });
