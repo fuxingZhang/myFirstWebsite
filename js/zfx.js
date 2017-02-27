d8348e26f5a5d57b86005dc571fd49a374d00b63
fuxingZhang <502545703@qq.com>
2017-02-27 17:42:38 +0800

    201702271742

--- js/zfx.js
+++ js/zfx.js
@@ -9,15 +9,55 @@ $(function(){
 
 	// banner  横幅; 标语; 旗，旗帜; 大字标题;
 	$(".banner").hover(function () {
-        $(this).find(".prev,.next").fadeTo("show", 0.1);
+        $(this).find(".prev,.next").fadeTo("slow", 0.5);
     }, function () {
         $(this).find(".prev,.next").hide();
     })
     $(".prev,.next").hover(function () {
-        $(this).fadeTo("show", 0.7);
+        $(this).fadeTo("normal", 0.7);
     }, function () {
-        $(this).fadeTo("show", 0.1);
+        $(this).fadeTo("normal", 0.5);
     })
-    $(".banner").slide({ titCell: ".num ul", mainCell: ".51buypic", effect: "left", autoPlay: true, delayTime: 700, autoPage: "<li><a></a></li>" });
-
+    
+    
+	$(".prev").click(function(){
+		// 获取当前的page
+		var page=$(".banner .num .on").attr("title");   
+		// 判断是否在动画
+		if (!$(".pic").is(":animated")) {
+			if (page==2){
+				page=0;
+				$(".pic").animate({left:'0px'},1000);
+				$(".banner .num .on").removeClass("on").prev().prev().addClass("on");
+			}else{
+				page++
+				$(".pic").animate({left:'-='+'1920px'},"slow");
+				$(".banner .num .on").removeClass("on").next().addClass("on");
+			}
+    		
+    	}
+    });
+	$(".next").click(function(){
+		var page=$(".banner .num .on").attr("title");
+		if (!$(".pic").is(":animated")) {
+			if (page==0){
+				page=2;
+				$(".pic").animate({left:'-3840px'},1000);
+				$(".banner .num .on").removeClass("on").next().next().addClass("on");
+			}else{
+				page--
+				$(".pic").animate({left:'+='+'1920px'},"slow");
+				$(".banner .num .on").removeClass("on").prev().addClass("on");
+			}
+    		
+    	}
+    });
+ 
+    $(".banner .num li").hover(function(){
+    	var n=$(this).attr("title")*1920;
+    	// alert(n);
+    	$(this).addClass("on")
+    		.siblings().removeClass("on");
+    	$(".pic").animate({left:-n+'px'},"normal");
+    })
 });
