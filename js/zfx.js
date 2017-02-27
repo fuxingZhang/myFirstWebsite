18996713b035efdb669cc9467acd62127e9fde1e
fuxingZhang <502545703@qq.com>
2017-02-27 22:35:58 +0800

    20170227

--- js/zfx.js
+++ js/zfx.js
@@ -8,19 +8,25 @@ $(function(){
 	});
 
 	// banner  横幅; 标语; 旗，旗帜; 大字标题;
-	$(".banner").hover(function () {
-        $(this).find(".prev,.next").fadeTo("slow", 0.5);
-    }, function () {
-        $(this).find(".prev,.next").hide();
-    })
-    $(".prev,.next").hover(function () {
-        $(this).fadeTo("normal", 0.7);
-    }, function () {
-        $(this).fadeTo("normal", 0.5);
-    })
+
+    $(".prev").click(function(){
+		var page=$(".banner .num .on").attr("title");
+		if (!$(".pic").is(":animated")) {
+			if (page==0){
+				page=2;
+				$(".pic").animate({left:'-3840px'},1000);
+				$(".banner .num .on").removeClass("on").next().next().addClass("on");
+			}else{
+				page--
+				$(".pic").animate({left:'+='+'1920px'},"slow");
+				$(".banner .num .on").removeClass("on").prev().addClass("on");
+			}
+    		
+    	}
+    });
     
-    
-	$(".prev").click(function(){
+
+	$(".next").click(function(){
 		// 获取当前的page
 		var page=$(".banner .num .on").attr("title");   
 		// 判断是否在动画
@@ -37,22 +43,27 @@ $(function(){
     		
     	}
     });
-	$(".next").click(function(){
-		var page=$(".banner .num .on").attr("title");
-		if (!$(".pic").is(":animated")) {
-			if (page==0){
-				page=2;
-				$(".pic").animate({left:'-3840px'},1000);
-				$(".banner .num .on").removeClass("on").next().next().addClass("on");
-			}else{
-				page--
-				$(".pic").animate({left:'+='+'1920px'},"slow");
-				$(".banner .num .on").removeClass("on").prev().addClass("on");
-			}
-    		
-    	}
-    });
- 
+
+	var timer=setInterval(function(){
+			$(".next").trigger("click");
+		},3000);
+
+	$(".banner").hover(function () {
+        $(this).find(".prev,.next").fadeTo("slow", 0.5);
+        clearInterval(timer);
+    }, function () {
+        $(this).find(".prev,.next").hide();
+        timer=setInterval(function(){
+			$(".next").trigger("click");
+		},3000);
+    })
+
+    $(".prev,.next").hover(function () {
+        $(this).fadeTo("normal", 0.8);
+    }, function () {
+        $(this).fadeTo("normal", 0.5);
+    })
+
     $(".banner .num li").hover(function(){
     	var n=$(this).attr("title")*1920;
     	// alert(n);
